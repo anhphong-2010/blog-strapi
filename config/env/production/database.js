771d294648828a4c91609f064085248bc1eeecb3
anhphong-2010 <anhphongnw.2010@gmail.com>
2022-03-24 10:37:32 +0700

    ^ config environment

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,23 +1,28 @@
-module.exports = ({ env }) => ({
-  defaultConnection: "default",
-  connections: {
-    default: {
-      connector: "bookshelf",
-      settings: {
-        client: "postgres",
-        host: env("DATABASE_HOST", "localhost"),
-        port: env.int("DATABASE_PORT", 5432),
-        database: env("DATABASE_NAME", "strapi"),
-        username: env("DATABASE_USERNAME", "strapi"),
-        password: env("DATABASE_PASSWORD", "strapi"),
-        schema: "public",
-        ssl: {
-          rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
+const parse = require("pg-connection-string").parse;
+
+module.exports = ({ env }) => {
+  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+  return {
+    defaultConnection: "default",
+    connections: {
+      default: {
+        connector: "bookshelf",
+        settings: {
+          client: "postgres",
+          host,
+          port,
+          database,
+          user,
+          password,
+          schema: "public",
+          ssl: {
+            rejectUnauthorized: { rejectUnauthorized: false },
+          },
+        },
+        options: {
+          ssl: env.bool("DATABASE_SSL", false),
         },
       },
-      options: {
-        ssl: env.bool("DATABASE_SSL", false),
-      },
     },
-  },
-});
+  };
+};
